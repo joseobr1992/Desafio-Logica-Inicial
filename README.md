7a3941200e65dae03a59bc49016b31e499aa47e7
Jose Carlos <joseobr1992@hotmail.com>
2024-05-29 08:43:17 -0300

    Entrega do projeto
    Projeto DIO

--- README.md
+++ README.md
@@ -3,4 +3,26 @@
 
 Este é um desafio da DIO referente ao curso [Formação em Lógica de Programação](https://web.dio.me/track/formacao-logica-de-programacao), este desafio foi feito usando JavaScript
 
+# Desafio Classificador de nível de Herói
+
+**O que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+
+## Objetivo
+
+Crie uma variável para armazenar o nome e a quantidade de experiência(XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+Se XP for menor que 1.000 = Ferro
+Se XP for entre 1.001 e 2.000 = Bronze
+Se XP for entre 2.001 e 5.000 = Prata 
+Se XP for entre 6.001 a 7.000 = Ouro
+Se XP for entre 7.001 e 8.000 = Platina
+Se XP for entre 8.001 e 9.000 = Ascendente
+Se XP for entre 9.001 e 10.000 = Imortal
+Se XP for maior ou igual a 10.001 = Radiante
+
 
--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+/*
+Desafio Classificador de nível de Herói
+*/
 
-console.log("ola mundo")
+let nomeHeroi = "José"
+let fase = 1
+let repeteFase = 1000
+let XP = fase * repeteFase
+const eloHeroi = ["Ferro","Bronze","Prata","Ouro","Platina","Ascendente","Imortal","Radiante"]
+
+
+if (XP < 1000) {
+    console.log(`${nomeHeroi} seu elo é ${eloHeroi[0]}`)
+} else if (XP >= 1001 && XP <= 2000) {
+    console.log(`${nomeHeroi} seu elo é ${eloHeroi[1]}`)
+} else if (XP >= 2001 && XP <= 5000) {
+    console.log(`${nomeHeroi} seu elo é ${eloHeroi[2]}`)
+} else if (XP >= 5001 && XP <= 7000) {
+    console.log(`${nomeHeroi} seu elo é ${eloHeroi[3]}`)
+} else if (XP >= 7001 && XP <= 8000) {
+    console.log(`${nomeHeroi} seu elo é ${eloHeroi[4]}`)
+} else if (XP >= 8001 && XP <= 9000) {
+    console.log(`${nomeHeroi} seu elo é ${eloHeroi[5]}`)
+} else if (XP >= 9001 && XP <= 10000) {
+    console.log(`${nomeHeroi} seu elo é ${eloHeroi[6]}`)
+} else {
+    console.log(`${nomeHeroi} seu elo é ${eloHeroi[7]}`)
+}
